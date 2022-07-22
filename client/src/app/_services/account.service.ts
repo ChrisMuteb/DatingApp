2a9b747d16c078ece8c1ea981cb13c0fbf44d670
ChrisMuteb <ibond.chris@gmail.com>
2022-07-22 14:28:38 +0300

    End of section 9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,13 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+// import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
+// environment.apiUrl
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';//inject our https client into our service
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();//bcz it going to be an observable place the $signe
 
